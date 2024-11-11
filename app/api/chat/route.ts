4231d5e794ab7292d799b57dd1d0b0aa476142c1
allen781105 <angela781105@gmail.com>
2024-11-11 10:42:32 +0800

    提交·代码

---
+++ app/api/chat/route.ts
@@ -0,0 +1,19 @@
+import { openai } from '@ai-sdk/openai';
+import { groq } from '@ai-sdk/groq';
+
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: openai('gpt-4-turbo'),
+    //model: groq('llama3-8b-8192'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
